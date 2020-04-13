8af59d4b1d1f7096d8b0f3ae1a12f7a820baee76
geminerald <zacdwarner@gmail.com>
2020-04-13 18:46:56 +0000

    addeed sweetalert and modal to sign in button

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("app.js is linked successfully");
+$(document).ready(function () {
+    console.log("app.js is linked successfully");
+
+});
+const loginModal = document.getElementById('login-modal');
+
+loginModal.addEventListener('click', () => {
+    Swal.fire({
+        icon: 'info',
+        title: 'Sign In',
+        input: 'email',
+        inputPlaceholder: 'Enter your email address',
+        footer: '<a href>Why do I have this issue?</a>'
+    })
+});
