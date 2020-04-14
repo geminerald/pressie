c4b0e1e6bc7c6b0f42f6c99becb4dae32a96aca8
geminerald <zacdwarner@gmail.com>
2020-04-14 15:01:50 +0000

    added basic login to index.html and app.js

--- app.js
+++ app.js
@@ -8,5 +8,39 @@ const signUpModal = document.getElementById('sign-up-modal');
 const loginModal = document.getElementById('login-modal');
 const signInOrUp = document.getElementById('sign-in-up-list');
 
+const logins = []
 
+const addLogin = (email) => {
+    logins.push(email);
+}
 
+signUpModal.addEventListener('click', () => {
+    Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'Next &rarr;',
+        showCancelButton: true,
+        progressSteps: ['1', '2']
+    }).queue([{
+            title:'Step 1',
+            text: 'Enter your email',
+            input: 'email'
+        },
+        {
+            title: 'Step 2',
+            text: 'Enter your password',
+            input: 'password'
+        }
+    ]).then((result) => {
+        if (result.value) {
+            const answers = JSON.stringify(result.value)
+            addLogin(answers)
+            Swal.fire({
+                title: 'All done!',
+                confirmButtonText: 'Lovely!',
+                
+            })
+        }
+    })
+})
+
+console.log(logins);
