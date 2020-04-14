fb006f61c041259729081a1fbdc660a5877c4163
geminerald <zacdwarner@gmail.com>
2020-04-14 08:51:54 +0000

    added dropdown for login menu

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ $(document).ready(function () {
     console.log("app.js is linked successfully");
 
 });
-const loginModal = document.getElementById('login-modal');
 
-loginModal.addEventListener('click', () => {
-    Swal.fire({
-        icon: 'info',
-        title: 'Sign In',
-        input: 'email',
-        inputPlaceholder: 'Enter your email address',
-        footer: '<a href>Why do I have this issue?</a>'
-    })
-});
+
+const signUpModal = document.getElementById('sign-up-modal');
+const loginModal = document.getElementById('login-modal');
+const signInOrUp = document.getElementById('sign-in-up-list');
+
+
+
