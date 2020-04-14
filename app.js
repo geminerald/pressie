cb719f01d922a794e163821d19b215e5aa11bbc4
geminerald <zacdwarner@gmail.com>
2020-04-14 15:15:33 +0000

    added basic login to existing functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ signUpModal.addEventListener('click', () => {
         showCancelButton: true,
         progressSteps: ['1', '2']
     }).queue([{
-            title:'Step 1',
+            title: 'Step 1',
             text: 'Enter your email',
             input: 'email'
         },
@@ -37,10 +37,47 @@ signUpModal.addEventListener('click', () => {
             Swal.fire({
                 title: 'All done!',
                 confirmButtonText: 'Lovely!',
-                
+
             })
         }
     })
 })
 
 console.log(logins);
+
+loginModal.addEventListener('click', () => {
+    Swal.mixin({
+        input: 'text',
+        confirmButtonText: 'Next &rarr;',
+        showCancelButton: true,
+        progressSteps: ['1', '2']
+    }).queue([{
+            title: 'Step 1',
+            text: 'Enter your email',
+            input: 'email'
+        },
+        {
+            title: 'Step 2',
+            text: 'Enter your password',
+            input: 'password'
+        }
+    ]).then((result) => {
+            if (result.value) {
+                const answers = JSON.stringify(result.value)
+                if (logins.includes(answers)) {
+                    Swal.fire({
+                        title: 'All done!',
+                        confirmButtonText: 'Lovely!',
+
+                    })
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Incorrect Email or Password',
+                        footer: '<a href>Why do I have this issue?</a>'
+                      })
+                }
+        }
+    })
+});
