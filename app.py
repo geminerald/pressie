5751a5a335b1c101399d730a6a70acfaf0de4d24
geminerald <zacdwarner@gmail.com>
2020-04-21 20:06:02 +0000

    added first half of app.py base

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'pressie'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser1@zwcluster1-znjzd.pressie.net/bugzap?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+
