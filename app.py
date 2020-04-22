ec6e90136c5866b591a3abd3bc6a6e402d467dd0
geminerald <zacdwarner@gmail.com>
2020-04-22 08:14:42 +0000

    moved db logins to env.py file and linked from app.py file

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'pressie'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser1@zwcluster1-znjzd.pressie.net/bugzap?retryWrites=true&w=majority'
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')         
 
 mongo = PyMongo(app)
 
