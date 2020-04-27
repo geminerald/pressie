0c4f463db352cd1ac667a61bfdf3e398cadc6858
geminerald <zacdwarner@gmail.com>
2020-04-27 09:55:30 +0000

    resolved bug where email validation error was appearing in password field, error in forms.py file, validation applied to incorrect filed

--- forms.py
+++ forms.py
@@ -6,8 +6,8 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
                            DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
                                      DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
