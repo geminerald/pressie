112c8677e2b7a2e246574ac387c59fd74726e770
geminerald <zacdwarner@gmail.com>
2020-05-02 20:59:28 +0000

    added login manager user loader function and softened border radius

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 @app.route('/home')
