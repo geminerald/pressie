2de04ff3537a5c78527291d5f18af3421b23d86b
geminerald <zacdwarner@gmail.com>
2020-05-02 22:32:30 +0000

    updated user loader route

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ login_manager = LoginManager(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.get(user_id)
 
 @app.route('/')
 @app.route('/home')
