a43f0a955963f1ff32af994df2bdefd57db720bc
geminerald <zacdwarner@gmail.com>
2020-05-14 22:35:46 +0000

    added alert to finder as well and implemented logic to ensure they don't throw error

--- static/js/app.js
+++ static/js/app.js
@@ -2,17 +2,34 @@ $(document).ready(function () {
     console.log("app.js is linked successfully");
 
 
+    const wishSwitch = document.getElementById('wishlist-switch');
+    const filterSearch = document.getElementById('finder-search')
+    if (wishSwitch) {
+        wishSwitch.addEventListener('click', () => {
 
+
+            Swal.fire(
+                'Hi There',
+                'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br><br> Apologies! ',
+                'info'
+            )
+        });
+    }
+
+
+    if (filterSearch) {
+
+    filterSearch.addEventListener('click', () => {
+        Swal.fire(
+            'Hi There',
+            'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br><br> Apologies! ',
+            'info'
+        )
+    });
+
+    }
 });
 
-
-document.getElementById('wishlist-switch').addEventListener('click', () => {
-    Swal.fire(
-        'Hi There',
-        'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br> Apologies! ',
-        'info'
-    )
-});
 
 
 /**
