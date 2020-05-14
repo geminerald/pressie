9d0fd0a49c39b81252a1b8bf397838d36bb4144f
geminerald <zacdwarner@gmail.com>
2020-05-14 22:28:39 +0000

    added alert to notify users of beta component

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
     console.log("app.js is linked successfully");
+
+
+
 });
+
+
+document.getElementById('wishlist-switch').addEventListener('click', () => {
+    Swal.fire(
+        'Hi There',
+        'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br> Apologies! ',
+        'info'
+    )
+});
+
+
+/**
+ *
+ * Swal.fire(
+  'The Internet?',
+  'That thing is still around?',
+  'question'
+)
+ *
+ */
