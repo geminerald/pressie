75a035a4c276db56478049756952eb119e4b79df
geminerald <zacdwarner@gmail.com>
2020-05-16 15:33:46 +0000

    added responsive navbar for smaller screen

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
     console.log("app.js is linked successfully");
 
+    const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+                }
+            });
+            burger.classList.toggle('toggle');
+        });
+    }
+
+    navSlide();
 
     const wishSwitch = document.getElementById('wishlist-switch');
     const filterSearch = document.getElementById('finder-search')
@@ -19,13 +38,13 @@ $(document).ready(function () {
 
     if (filterSearch) {
 
-    filterSearch.addEventListener('click', () => {
-        Swal.fire(
-            'Hi There',
-            'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br><br> Apologies! ',
-            'info'
-        )
-    });
+        filterSearch.addEventListener('click', () => {
+            Swal.fire(
+                'Hi There',
+                'This feature is currently in Beta and non operational. It will be implemented in Final Release. <br><br> Apologies! ',
+                'info'
+            )
+        });
 
     }
 });
